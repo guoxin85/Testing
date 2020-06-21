62ba08487109c7524cd7070bc408f1fba82da9c1
Anton <getgoodapp@gmail.com>
2020-06-21 23:13:10 +0800

    new, create, show, edit, update, destroy functionalities

--- app/controllers/testapps_controller.rb
+++ app/controllers/testapps_controller.rb
@@ -1,7 +1,51 @@
 class TestappsController < ApplicationController
 
   def index
-    @testapp_items = Testapp.all
+    @testItems = Testapp.all
+  end
+
+  def show
+    @testItem = Testapp.find(params[:id])
+  end
+
+  def new
+    @testItem = Testapp.new
+  end
+
+  def create
+    @testItem = Testapp.new(params.require(:testapp).permit(:title, :body))
+    respond_to do |format|
+      if @testItem.save
+        format.html { redirect_to testapps_path, notice: "" }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @testItem = Testapp.find(params[:id])
+  end
+
+  def update
+    @testItem = Testapp.find(params[:id])
+
+    respond_to do |format|
+      if @testItem.update(params.require(:testapp).permit(:title, :body))
+        format.html { redirect_to testapps_path, notice: ""}
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+def destroy
+    @testItem = Testapp.find(params[:id])
+
+    @testItem.destroy
+    respond_to do |format|
+      format.html { redirect_to testapps_path, notice: "" }
+    end
   end
 
 end
